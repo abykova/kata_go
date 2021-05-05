e1edf9fdaf0d28aca487ce73ab473aee6ac06f71
abykova <dbad@technodom.kz>
2021-05-05 14:40:27 +0600

    ref

--- kyu7/angle.go
+++ kyu7/angle.go
@@ -1,7 +1,6 @@
-package kata
+package kyu7
 
 // Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
-
 func Angle(n int) int {
 	return 180 * (n - 2)
 }
